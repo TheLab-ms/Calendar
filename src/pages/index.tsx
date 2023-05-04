c033c746c6438e5f042485e1358a8e2475d5a95a
alessioharo <alessio.haro@icloud.com>
2023-05-04 15:28:55 -0500

    Exported the Index Page and Calendar components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
-import Calendar from '@/components/Calendar';
 import Layout from '../components/layout';
+import Calendar from '../components/Calendar';
+import { prisma } from '@/helpers/db';
+import { GetServerSideProps } from 'next';
+import { EventInput } from '@fullcalendar/core';
 
-export default function IndexPage() {
+export const getServerSideProps: GetServerSideProps = async () => {
+	const events = await prisma.event.findMany({});
+	const conformedEvents = events.map((calEvent) => {
+		const fullCalEvent: EventInput = {
+			title: calEvent.title,
+			start: calEvent.startTime.toISOString(),
+			end: calEvent.endTime.toISOString(),
+		};
+		return fullCalEvent;
+	});
+	return {
+		props: {
+			events: conformedEvents,
+		},
+	};
+};
+
+export default function IndexPage({ events }: { events: EventInput[] }) {
 	return (
 		<Layout>
-			<div style={{ width: '100%' }}>
-				<br></br>
-				<Calendar />
-				<br></br>
-				<br></br>
-			</div>
+			<Calendar events={events} />
 		</Layout>
 	);
 }
