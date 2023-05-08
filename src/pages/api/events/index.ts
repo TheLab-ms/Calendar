77fca310c3230fd5cc2867ceb9b9a6fc6a20e3dc
Cameron Steele <cam.steeleis@gmail.com>
2023-05-08 18:50:04 -0500

    Merge create events and get events into /api/events endpoint

--- src/pages/api/events/index.ts
+++ src/pages/api/events/index.ts
@@ -1,10 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/helpers/db';
 
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
+	if (req.method === 'GET') {
+		const currentDate = new Date();
+		const events = await prisma.event.findMany({
+			where: { startTime: { gt: currentDate } },
+			orderBy: { startTime: 'asc' },
+		});
+
+		res.status(200).json(events);
+	}
 	// Route implementation will go here
 	if (req.method === 'POST') {
 		// Parse the request body to get the event data
@@ -14,8 +23,6 @@ export default async function handler(
 		const modifiedEventData = { ...eventData };
 		modifiedEventData.startTime = new Date(eventData.startTime);
 		modifiedEventData.endTime = new Date(eventData.endTime);
-		// Create a new PrismaClient instance
-		const prisma = new PrismaClient();
 
 		// Use Prisma to create the event in the database
 		const newEvent = await prisma.event.create({
